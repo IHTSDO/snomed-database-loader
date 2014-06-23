79848bb676c975f0fc6502ef531abfadb131d0bf
Eligio Becerra-Zavala <ebecerra@lanetelecom.com>
2014-06-23 16:40:23 +0100

    Started reconstructing tables with foreign keys and unique constrains

--- environment.sql
+++ environment.sql
@@ -1,92 +1,71 @@
 /* create the database */
-drop schema if exists snomedct;
-create schema if not exists snomedct;
+drop schema if exists 'snomedct';
+create schema if not exists 'snomedct';
 set schema 'snomedct';
 
 /* create the Full S-CT data tables */
 
 drop table if exists curr_concept_f;
 create table curr_concept_f(
-id varchar(18) not null,
-effectivetime char(8) not null,
-active char(1) not null,
-moduleid varchar(18) not null,
-definitionstatusid varchar(18) not null,
-key idx_id(id),
-key idx_effectivetime(effectivetime),
-key idx_active(active),
-key idx_moduleid(moduleid),
-key idx_definitionstatusid(definitionstatusid)
+primary key id varchar(18) not null,
+unique effectivetime char(8) not null,
+unique active char(1) not null,
+unique moduleid varchar(18) not null,
+unique definitionstatusid varchar(18) not null
 );
-
 
 drop table if exists curr_description_f;
 create table curr_description_f(
-id varchar(18) not null,
-effectivetime char(8) not null,
-active char(1) not null,
-moduleid varchar(18) not null,
-conceptid varchar(18) not null,
-languagecode varchar(2) not null,
-typeid varchar(18) not null,
-term varchar(255) not null,
-casesignificanceid varchar(18) not null,
-key idx_id(id),
-key idx_effectivetime(effectivetime),
-key idx_active(active),
-key idx_moduleid(moduleid),
-key idx_conceptid(conceptid),
-key idx_languagecode(languagecode),
-key idx_typeid(typeid),
-key idx_term(term),
-key idx_casesignificanceid(casesignificanceid)
+id varchar(18) not null references curr_concept_f(id),
+effectivetime char(8) not null  references curr_concept_f(id),
+active char(1) not null  references curr_concept_f(id),
+moduleid varchar(18) not null  references curr_concept_f(id),
+unique conceptid varchar(18) not null,
+unique languagecode varchar(2) not null,
+unique typeid varchar(18) not null,
+unique term varchar(255) not null,
+unique casesignificanceid varchar(18) not null
 );
 
 drop table if exists curr_textdefinition_f;
 create table curr_textdefinition_f(
-id varchar(18) not null,
-effectivetime char(8) not null,
-active char(1) not null,
-moduleid varchar(18) not null,
-conceptid varchar(18) not null,
-languagecode varchar(2) not null,
-typeid varchar(18) not null,
-term varchar(1024) not null,
-casesignificanceid varchar(18) not null,
-key idx_id(id),
-key idx_effectivetime(effectivetime),
-key idx_active(active),
-key idx_moduleid(moduleid),
-key idx_conceptid(conceptid),
-key idx_languagecode(languagecode),
-key idx_typeid(typeid),
-key idx_term(term(255)),
-key idx_casesignificanceid(casesignificanceid)
+id varchar(18) not null references curr_concept_f(id),
+effectivetime char(8) not null references curr_concept_f(id),
+active char(1) not null references curr_concept_f(id),
+moduleid varchar(18) not null references curr_concept_f(id),
+conceptid varchar(18) not null references curr_description_f(conceptid),
+languagecode varchar(2) not null references curr_description_f(conceptid),
+typeid varchar(18) not null references curr_description_f(conceptid),
+unique term varchar(1024) not null,
+casesignificanceid varchar(18) not null references curr_description_f(conceptid)
 );
 
 drop table if exists curr_relationship_f;
 create table curr_relationship_f(
-id varchar(18) not null,
-effectivetime char(8) not null,
-active char(1) not null,
-moduleid varchar(18) not null,
-sourceid varchar(18) not null,
-destinationid varchar(18) not null,
-relationshipgroup varchar(18) not null,
-typeid varchar(18) not null,
-characteristictypeid varchar(18) not null,
-modifierid varchar(18) not null,
-key idx_id(id),
-key idx_effectivetime(effectivetime),
-key idx_active(active),
-key idx_moduleid(moduleid),
-key idx_sourceid(sourceid),
-key idx_destinationid(destinationid),
-key idx_relationshipgroup(relationshipgroup),
-key idx_typeid(typeid),
-key idx_characteristictypeid(characteristictypeid),
-key idx_modifierid(modifierid)
+id varchar(18) not null references curr_concept_f(id),
+effectivetime char(8) not null references curr_concept_f(id),
+active char(1) not null references curr_concept_f(id),
+moduleid varchar(18) not null references curr_concept_f(id),
+unique sourceid varchar(18) not null,
+unique destinationid varchar(18) not null,
+unique relationshipgroup varchar(18) not null,
+typeid varchar(18) not null references curr_description_f(conceptid),
+unique characteristictypeid varchar(18) not null,
+unique modifierid varchar(18) not null
 );
+
+/*
+id varchar(18) not null references curr_concept_f(id),
+effectivetime char(8) not null references curr_concept_f(id),
+active char(1) not null references curr_concept_f(id),
+moduleid varchar(18) not null references curr_concept_f(id),
+conceptid varchar(18) not null references curr_description_f(conceptid),
+languagecode varchar(2) not null references curr_description_f(conceptid),
+typeid varchar(18) not null references curr_description_f(conceptid),
+term varchar(1024) not null references curr_description_f(conceptid),
+casesignificanceid varchar(18) not null references curr_description_f(conceptid)
+references curr_relationship_f(typeid)
+*/
 
 drop table if exists curr_stated_relationship_f;
 create table curr_stated_relationship_f(
@@ -97,19 +76,9 @@ moduleid varchar(18) not null,
 sourceid varchar(18) not null,
 destinationid varchar(18) not null,
 relationshipgroup varchar(18) not null,
-typeid varchar(18) not null,
+typeid varchar(18) not null references curr_relationship_f(typeid),
 characteristictypeid varchar(18) not null,
-modifierid varchar(18) not null,
-key idx_id(id),
-key idx_effectivetime(effectivetime),
-key idx_active(active),
-key idx_moduleid(moduleid),
-key idx_sourceid(sourceid),
-key idx_destinationid(destinationid),
-key idx_relationshipgroup(relationshipgroup),
-key idx_typeid(typeid),
-key idx_characteristictypeid(characteristictypeid),
-key idx_modifierid(modifierid)
+modifierid varchar(18) not null
 );
 
 drop table if exists curr_langrefset_f;
@@ -120,14 +89,7 @@ active char(1) not null,
 moduleid varchar(18) not null,
 refsetid varchar(18) not null,
 referencedcomponentid varchar(18) not null,
-acceptabilityid varchar(18) not null,
-key idx_id(id),
-key idx_effectivetime(effectivetime),
-key idx_active(active),
-key idx_moduleid(moduleid),
-key idx_refsetid(refsetid),
-key idx_referencedcomponentid(referencedcomponentid),
-key idx_acceptabilityid(acceptabilityid)
+acceptabilityid varchar(18) not null
 );
 
 drop table if exists curr_associationrefset_d;
@@ -138,14 +100,7 @@ active char(1) not null,
 moduleid varchar(18) not null,
 refsetid varchar(18) not null,
 referencedcomponentid varchar(18) not null,
-targetcomponentid varchar(18) not null,
-key idx_id(id),
-key idx_effectivetime(effectivetime),
-key idx_active(active),
-key idx_moduleid(moduleid),
-key idx_refsetid(refsetid),
-key idx_referencedcomponentid(referencedcomponentid),
-key idx_targetcomponentid(targetcomponentid)
+targetcomponentid varchar(18) not null
 );
 
 drop table if exists curr_attributevaluerefset_f;
@@ -156,14 +111,7 @@ active char(1) not null,
 moduleid varchar(18) not null,
 refsetid varchar(18) not null,
 referencedcomponentid varchar(18) not null,
-valueid varchar(18) not null,
-key idx_id(id),
-key idx_effectivetime(effectivetime),
-key idx_active(active),
-key idx_moduleid(moduleid),
-key idx_refsetid(refsetid),
-key idx_referencedcomponentid(referencedcomponentid),
-key idx_valueid(valueid)
+valueid varchar(18) not null
 );
 
 drop table if exists curr_simplemaprefset_f;
@@ -174,14 +122,7 @@ active char(1) not null,
 moduleid varchar(18) not null,
 refsetid varchar(18) not null,
 referencedcomponentid varchar(18) not null,
-maptarget varchar(32) not null,
-key idx_id(id),
-key idx_effectivetime(effectivetime),
-key idx_active(active),
-key idx_moduleid(moduleid),
-key idx_refsetid(refsetid),
-key idx_referencedcomponentid(referencedcomponentid),
-key idx_maptarget(maptarget)
+maptarget varchar(32) not null
 );
 
 drop table if exists curr_simplerefset_f;
@@ -191,13 +132,7 @@ create table curr_simplerefset_f(
 	active char(1) not null,
 	moduleid varchar(18) not null,
 	refsetid varchar(18) not null,
-	referencedcomponentid varchar(18) not null,
-	key idx_id(id),
-	key idx_effectivetime(effectivetime),
-	key idx_active(active),
-	key idx_moduleid(moduleid),
-	key idx_refsetid(refsetid),
-	key idx_referencedcomponentid(referencedcomponentid)
+	referencedcomponentid varchar(18) not null
 );
 
 drop table if exists curr_complexmaprefset_f;
@@ -213,12 +148,5 @@ create table curr_complexmaprefset_f(
 	mapRule varchar(18),
 	mapAdvice varchar(18),
 	mapTarget varchar(18),
-	correlationId varchar(18) not null,
-	key idx_id(id),
-	key idx_effectivetime(effectivetime),
-	key idx_active(active),
-	key idx_moduleid(moduleid),
-	key idx_refsetid(refsetid),
-	key idx_referencedcomponentid(referencedcomponentid),
-	key idx_mapTarget(mapTarget)
+	correlationId varchar(18) not null
 );
