cca151171a9a1c177312309394fcffd8b1efb829
siamchen <jeffrey@hiis.org>
2015-10-13 14:35:02 +1100

    Add comment
    This is not a solution to create a blank database for snomedct

--- create-database-postgres.sql
+++ create-database-postgres.sql
@@ -1,4 +1,6 @@
 /* Create the database */
+-- This is to create a schema inside a database instead of creating a blank database.
+-- Postgres does not allow to drop a database within the same connection. Also, postgres does not allow switch database within sql statement.
 drop schema if exists snomedct;
 create schema if not exists snomedct;
 set schema 'snomedct';
