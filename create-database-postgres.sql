d0b17e596287e1f9275a2068c6970bc823fdf071
Jeffrey Chen <jeffrey@hiis.org>
2015-10-14 09:54:46 +1100

    Add cascade for dropping snomedct schema.

--- create-database-postgres.sql
+++ create-database-postgres.sql
@@ -1,6 +1,7 @@
 /* Create the database */
 -- This is to create a schema inside a database instead of creating a blank database.
--- Postgres does not allow to drop a database within the same connection. Also, postgres does not allow switch database within sql statement.
-drop schema if exists snomedct;
-create schema if not exists snomedct;
+-- Postgres does not allow to drop a database within the same connection. 
+-- Also, postgres does not allow switch database within sql statement. A new connection must be estabilished for changing database target.
+drop schema if exists snomedct cascade;
+create schema snomedct;
 set schema 'snomedct';
