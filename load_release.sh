d431fca2616c0d08c69c4763bd84883d65303ff9
Peter G. Williams <pwi@ihtsdo.org>
2015-12-10 16:14:30 +0000

    Add support for only creating/loading data type required Delta, Snapshot, Full or All

--- load_release.sh
+++ load_release.sh
@@ -3,26 +3,58 @@ set -e;
 
 releasePath=$1
 dbName=$2
+loadType=$3
 
-if [ -z ${dbName} ]
+if [ -z ${loadType} ]
 then
-	echo "Usage <release location> <db schema name>"
+	echo "Usage <release location> <db schema name> <DELTA|SNAP|FULL|ALL>"
 	exit -1
 fi
 
 #Unzip the files here, junking the structure
 localExtract="tmp_extracted"
-rm -rf $localExtract
-generatedScript="tmp_loader.sql"
-unzip -j ${releasePath} -d ${localExtract}
+generatedLoadScript="tmp_loader.sql"
+generatedEnvScript="tmp_environment-mysql.sql"
 
+#What types of files are we loading - delta, snapshot, full or all?
+case "${loadType}" in 
+	'DELTA') fileTypes=(Delta)
+		unzip -j ${releasePath} "*Delta*" -d ${localExtract}
+	;;
+	'SNAP') fileTypes=(Snapshot)
+		unzip -j ${releasePath} "*Snapshot*" -d ${localExtract}
+	;;
+	'FULL') fileTypes=(Full)
+		unzip -j ${releasePath} "*Full*" -d ${localExtract}
+	;;
+	'ALL') fileTypes=(Delta Snapshot Full)	
+		unzip -j ${releasePath} -d ${localExtract}
+	;;
+	*) echo "File load type ${loadType} not recognised"
+	exit -1;
+	;;
+esac
+
+	
 #Determine the release date from the filenames
-releaseDate=`ls -1 ${localExtract}/*.txt | head -1 | egrep -o '[0-9]{8}'`
+releaseDate=`ls -1 ${localExtract}/*.txt | head -1 | egrep -o '[0-9]{8}'`	
 
-fileTypes=(Delta Full Snapshot)
+#Generate the environemnt script by running through the template as 
+#many times as required
+now=`date +"%Y%m%d_%H%M%S"`
+echo "\nGenerating Environment script for ${loadType} type(s)"
+echo "/* Script Generated Automatically by load_release.sh ${now} */" > ${generatedEnvScript}
+for fileType in ${fileTypes[@]}; do
+	fileTypeLetter=`echo "${fileType}" | head -c 1 | tr '[:upper:]' '[:lower:]'`
+	tail -n +2 environment-mysql-template.sql | while read thisLine
+	do
+		echo "${thisLine/TYPE/${fileTypeLetter}}" >> ${generatedEnvScript}
+	done
+done
+
 function addLoadScript() {
 	for fileType in ${fileTypes[@]}; do
-		echo "load data local" >> ${generatedScript}
+		echo "load data local" >> ${generatedLoadScript}
 		fileName=${1/TYPE/${fileType}}
 		fileName=${fileName/DATE/${releaseDate}}
 
@@ -38,31 +70,34 @@ function addLoadScript() {
 
 		tableName=${2}_`echo $fileType | head -c 1 | tr '[:upper:]' '[:lower:]'`
 
-		echo "\tinfile '"${localExtract}/${fileName}"'" >> ${generatedScript}
-		echo "\tinto table ${tableName}" >> ${generatedScript}
-		echo "\tcolumns terminated by '\\\t'" >> ${generatedScript}
-		echo "\tlines terminated by '\\\r\\\n'" >> ${generatedScript}
-		echo "\tignore 1 lines;" >> ${generatedScript}
-		echo ""  >> ${generatedScript}
+		echo "\tinfile '"${localExtract}/${fileName}"'" >> ${generatedLoadScript}
+		echo "\tinto table ${tableName}" >> ${generatedLoadScript}
+		echo "\tcolumns terminated by '\\\t'" >> ${generatedLoadScript}
+		echo "\tlines terminated by '\\\r\\\n'" >> ${generatedLoadScript}
+		echo "\tignore 1 lines;" >> ${generatedLoadScript}
+		echo ""  >> ${generatedLoadScript}
+		echo "select 'Loaded ${fileName} into ${tableName}' as '  ';" >> ${generatedLoadScript}
+		echo ""  >> ${generatedLoadScript}
 	done
-
-
 }
 
-echo "Generating loading script for $releaseDate"
-echo "/* Generated Loader Script */" >  ${generatedScript}
+echo "\nGenerating loading script for $releaseDate"
+echo "/* Generated Loader Script */" >  ${generatedLoadScript}
 addLoadScript sct2_Concept_TYPE_INT_DATE.txt concept
 addLoadScript sct2_Description_TYPE-en_INT_DATE.txt description
 addLoadScript sct2_StatedRelationship_TYPE_INT_DATE.txt stated_relationship
 addLoadScript sct2_Relationship_TYPE_INT_DATE.txt relationship
 addLoadScript der2_cRefset_AttributeValueTYPE_INT_DATE.txt attributevaluerefset
 
-echo "Passing $generatedScript to MYSQL"
-
 mysql -u root  << EOF
-	drop database IF EXISTS ${dbName} ;
-	create database ${dbName};
-	use ${dbName}
-	source environment-mysql.sql
-	source ${generatedScript};
+	select 'Ensuring schema ${dbName} exists' as '  ';
+	create database IF NOT EXISTS ${dbName};
+	use ${dbName};
+	select '(re)Creating Schema using ${generatedEnvScript}' as '  ';
+	source ${generatedEnvScript};
+	select 'Loading RF2 Data using ${generatedLoadScript}' as '  ';
+	source ${generatedLoadScript};
 EOF
+
+rm -rf $localExtract
+#We'll leave the generated environment & load scripts for inspection
