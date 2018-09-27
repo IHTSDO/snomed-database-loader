b132a516152faed1150e2c291615b2deaa92dbd5
Jay Pedersen <jay.pedersen@unmc.edu>
2018-09-27 14:23:18 -0500

    neo4j_tools -- improve on run_cypher command, allow --cypher '<CYPHER code>' rather than absolutely require a file with CYPHER.

--- NEO4J/snomed_g_neo4j_tools.py
+++ NEO4J/snomed_g_neo4j_tools.py
@@ -11,43 +11,64 @@ Syntax and Semantics:
 '''
 
 def run_cypher(arglist):
-  # Parse command line
-  opt = optparse.OptionParser()
-  opt.add_option('--verbose',action='store_true')
-  opt.add_option('--neopw64', action='store')
-  opt.add_option('--neopw', action='store')
-  opts, args = opt.parse_args(arglist)
-  if not (len(args)==1 and (opts.neopw or opts.neopw64)):
-    print('Usage: command <cypherfile> --neopw <pw>'); sys.exit(1)
-  if opts.neopw and opts.neopw64:
-    print('Usage: only one of --neopw and --neopw64 may be specified')
-    sys.exit(1)
-  if opts.neopw64: # snomed_g v1.2, convert neopw64 to neopw
-    opts.neopw = str(base64.b64decode(opts.neopw64),'utf-8') if sys.version_info[0]==3 else base64.decodestring(opts.neopw64) # py2
+
+  def parse_command(arglist):
+    # Parse command line
+    opt = optparse.OptionParser()
+    opt.add_option('--verbose',action='store_true')
+    opt.add_option('--neopw64', action='store')
+    opt.add_option('--neopw', action='store')
+    opt.add_option('--cypher', action='store')
+    opts, args = opt.parse_args(arglist)
+    if not ((opts.neopw or opts.neopw64) and
+            ((len(args)==1 and not opts.cypher) or
+             (len(args)==0 and opts.cypher))):
+      print('Usage: command <cypherfile> --neopw <pw>'); sys.exit(1)
+    if opts.neopw and opts.neopw64:
+      print('Usage: only one of --neopw and --neopw64 may be specified')
+      sys.exit(1)
+    if opts.neopw64: # snomed_g v1.2, convert neopw64 to neopw
+      opts.neopw = str(base64.b64decode(opts.neopw64),'utf-8') if sys.version_info[0]==3 else base64.decodestring(opts.neopw64) # py2
+    return opts, args
+  # end parse_command
+
+  opts, args = parse_command(arglist)
   n4jpw = opts.neopw
-  cypher_fn = args[0]
-  graph_db = py2neo.Graph(password=n4jpw) # 'http://localhost:7474/db/data/transaction/commit')
-  # want LONG TIMEOUT for CSV loadering
+  try:
+    graph_db = py2neo.Graph(password=n4jpw) # 'http://localhost:7474/db/data/transaction/commit')
+  except Exception as e:
+    print('*** Failed to make connection to NEO4J database ***')
+    print('Exception: %s' % type(e).__name__)
+    print('Exception arguments: %s' % e.args)
+    sys.exit(1)
+  # want LONG TIMEOUT for CSV loading
   from py2neo.packages.httpstream import http
   http.socket_timeout = 1200 # JGP default was 30 on July 2016, this is 20 min I believe
   # Execute the CYPHER commands in the file
-  command_lines = [ x.rstrip('\n').rstrip('\r').strip() for x in open(cypher_fn) ]
+  if opts.cypher:
+    command_lines = [ opts.cypher ]
+  else:
+    cypher_fn = args[0]
+    command_lines = [ x.rstrip('\n').rstrip('\r').strip() for x in open(cypher_fn) ]
   succeeded, failed = 0, 0
   next_cmd = ''
   for idx,cmd in enumerate(command_lines):
     if opts.verbose: print('%d. %s' % (idx+1,cmd))
     if len(cmd)==0 or cmd.startswith('//'): continue # empty or comment
-    next_cmd += cmd if len(next_cmd)==0 else ' '+cmd
+    next_cmd += (cmd if len(next_cmd)==0 else (' '+cmd))
     if next_cmd.rstrip()[-1] != ';': continue # dont sent until semicolon ends sequence of commands
     # dont' send if a RETURN '<message>';, just check for RETURN ' for now -- works with our scripts
     if next_cmd.startswith("""RETURN '"""): next_cmd = ''; continue
     if opts.verbose: print('Sending CYPHER:[%s]' % next_cmd)
-    success = False
-    cursor = None
     command_start = datetime.datetime.now()
-    try:   cursor = graph_db.run(next_cmd).dump()
-    except Exception as e: failed += 1; print('*** DB failure: command %d [%s] : [%s,%s]' % (idx+1,next_cmd,type(e),str(e))); pass
-    else:  succeeded += 1; success = True
+    try:
+      graph_db.run(next_cmd).dump()
+    except Exception as e:
+      failed += 1
+      print('*** DB failure: command %d [%s] : [%s,%s]' % (idx+1,next_cmd,type(e),str(e)))
+      pass
+    else:
+      succeeded += 1
     if opts.verbose: command_end = datetime.datetime.now(); print('CYPHER execution time: %s' % (str(command_end-command_start),))
     next_cmd = ''
   print('SUCCESS (%d commands)' % succeeded if failed==0 else 'FAILED (%d commands failed, %d commands succeeded)' % (failed,succeeded) )
