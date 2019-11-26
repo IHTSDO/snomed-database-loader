0ddc48b8911c99ce4024acb2123ed65389ded1f4
dmarkwell <dma@ihtsdo.org>
2019-11-26 09:00:26 +0000

    Replaced mysql-loader-with-optimized-views with completely new version
    Old version of mysql-loader-with-optimized-views replaced with complete new version. The new version contains many enhancement including easy to use scripts for MacOS and Windows that rather necessary data from user, inserts these parameters in a temporary copy of the loader script and runs this script.
    The new version was developed on a separate GitHub branch in the SnomedRfsMySql folder. Detailed instruction on use are at http://snomed.org/snomedmysql

--- mysql-loader-with-optimized-views/PerformanceQueries/refset_attribs.sql
+++
@@ -1,7 +0,0 @@
-select LOWER(p.term),GROUP_CONCAT(CONCAT('{"name":"',LOWER(pd.term),'","type":"',LOWER(pt.term),'"}') order by attributeOrder)
-FROM soa_refset_RefsetDescriptor
-JOIN soa_pref pt ON pt.conceptId=attributeType
-JOIN soa_pref pd ON pd.conceptId=attributeDescription
-JOIN soa_pref p ON p.conceptId=referencedComponentId
-WHERE attributeOrder>0
-GROUP BY p.term;
